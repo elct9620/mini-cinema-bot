8c30f4f3f70e89e0038c53613f4d5055a5382307
蒼時弦也 <elct9620@frost.tw>
2021-03-22 11:00:56 +0800

    Add more movies reply options

--- lib/handler.rb
+++ lib/handler.rb
@@ -6,8 +6,15 @@ require_relative './carousel'
 class Handler
   # @since 0.1.0
   SHARED_HEADER = { 'Content-Type' => 'text' }
+
+  # @since 0.1.0
   SUCCESS_RESPONSE = [200, SHARED_HEADER, ["Success"]]
+
+  # @since 0.1.0
   BAD_REQUEST_RESPONSE = [400, SHARED_HEADER, ["Bad Request"]]
+
+  # @since 0.1.0
+  KEYWORD_REGEXP = /([^\s]*)\s*([0-9]+)?/
 
   # @since 0.1.0
   attr_reader :body, :movies
@@ -47,8 +54,10 @@ class Handler
   # @since 0.1.0
   def perform
     messages.each do |message|
-      movies = filtered_movies(message.dig('message', 'text')).take(10)
-      Bot.reply_message(message['replyToken'], Carousel.new(movies).to_h)
+      keyword, amount = message.dig('message', 'text').match(KEYWORD_REGEXP)[1..2]
+      movies = filtered_movies(keyword).take([10, amount.to_i].max)
+      reply = movies.each_slice(10).map { |movies_group| Carousel.new(movies_group).to_h }
+      Bot.reply_message(message['replyToken'], reply)
     end
 
     SUCCESS_RESPONSE
@@ -61,7 +70,8 @@ class Handler
   # @since 0.1.0
   def filtered_movies(text)
     case text
-    when /最新/ then movies.sort_by(&:date).reverse
+    when /最近/ then movies.reject { |movie| movie.date > DateTime.now }.sort_by(&:date).reverse
+    when /近期/ then movies.sort_by(&:date).reverse
     when /隨機/ then movies.shuffle
     else movies
     end
